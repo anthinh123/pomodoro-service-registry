d1df6bc712fa6158c258d98addda5051e68be2f9
thinh <anvanthinh@gmail.com>
2024-07-07 21:29:26 +0700

    init service registry

---
+++ src/main/java/com/thinh/pomodoroserviceregistry/PomodoroserviceregistryApplication.java
@@ -0,0 +1,13 @@
+package com.thinh.pomodoroserviceregistry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PomodoroserviceregistryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PomodoroserviceregistryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/thinh/pomodoroserviceregistry/PomodoroserviceregistryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thinh.pomodoroserviceregistry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PomodoroserviceregistryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
